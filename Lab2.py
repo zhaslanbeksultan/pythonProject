78ea5ec36b81e8beaffcd575db3aebf3c285b799
skkeb45 <b_zhaslan@kbtu.kz>
2023-01-24 17:08:42 +0600

    Lab 2.1

--- Lab2.py
+++ Lab2.py
@@ -361,3 +361,84 @@ myfamily = {
 }
 
 #While_Loops////////////////////////////////////////////////////////////////////////////////////////////////////////////
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+i = 1
+
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+#For_Loops//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+for x in "banana":
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "apple":
+    break
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+for x in range(6):
+  print(x)
+
+for x in range(2, 6):
+  print(x)
+
+for x in range(2, 30, 3):
+  print(x)
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+for x in [0, 1, 2]:
+  pass
